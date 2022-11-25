81a4a2425fd32d061f33bb284696545961e77e01
jjcastillot <jjcastillot@gmail.com>
2022-11-24 20:43:24 -0500

    COMMIT 2 - 24NOV22 Progress

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-from resources import MENU
-from resources import resources
+from resources import MENU, resources, logo
 
+# Main variables
+master_power = 'on'
+
+#Main display
+print("COFFEE MACHINE 3000")
+print(logo)
+print("Current resources:")
+print(f"Water: {resources['water']}\nMilk: {resources['milk']}")
+print(f"Coffee: {resources['coffee']}")
+choice = input('''
+What would you like today?
+A) Espresso
+B) Latte
+C) Capuccino 
+''').lower()
+
+if choice == 'off':
+    print("MACHINE TURNED OFF")
+    pass
+else:
+    print('bye')
